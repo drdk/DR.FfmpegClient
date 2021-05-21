b9350a3ca34b02ec8a992f03a9198fbc476d55c0
Bo Bendtsen <bben@dr.dk>
2021-05-21 08:52:04 +0200

    Increased frames to test from 100 to 300. Bumped nugets. Added parameter to change frames to test without rebuilding nuget.

--- src/DR.FfmpegUtil/Utils.cs
+++ src/DR.FfmpegUtil/Utils.cs
@@ -8,9 +8,9 @@ namespace DR.FfmpegUtil
 
     {
         private static string _ffmpegPath = $@"{AppDomain.CurrentDomain.BaseDirectory}\ffmpeg\ffmpeg.exe";
-        public static async Task<InterlaceTestResult> TestForInterlaced(string filePath)
+        public static async Task<InterlaceTestResult> TestForInterlaced(string filePath, int framesToTest = 300)
         {
-            using (var cmd = Command.Run(_ffmpegPath, options: o => o.StartInfo(i => i.Arguments = $"-filter:v idet -frames:v 100 -an -f rawvideo -y NUL -i \"{filePath}\"")))
+            using (var cmd = Command.Run(_ffmpegPath, options: o => o.StartInfo(i => i.Arguments = $"-filter:v idet -frames:v {framesToTest} -an -f rawvideo -y NUL -i \"{filePath}\"")))
             {
                 await cmd.Task;
                 return new InterlaceTestResult(cmd.Result.StandardError);
